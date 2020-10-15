26059950dd9ff49cc0076498bf10cb6562f9a005
i1-kmv <i1-kmv@mail.ru>
2020-10-15 10:55:32 +0200

    Updates

---
+++ precache-manifest.eeb200d89e4e8ff4570121140b31ea88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae5ec8376298283af89b57624ccf3cbd",
+    "url": "/HelloFriday/index.html"
+  },
+  {
+    "revision": "76aef51454580c431c84",
+    "url": "/HelloFriday/static/css/main.7f39c0fa.chunk.css"
+  },
+  {
+    "revision": "fa46ab21c3867ebaa3d2",
+    "url": "/HelloFriday/static/js/2.de41ce22.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/HelloFriday/static/js/2.de41ce22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76aef51454580c431c84",
+    "url": "/HelloFriday/static/js/main.27c88bbe.chunk.js"
+  },
+  {
+    "revision": "94b52624e74f313357d0",
+    "url": "/HelloFriday/static/js/runtime-main.351dbb84.js"
+  }
+]);
